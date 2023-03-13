e25e28b5af7467b557717d03359149a95016a6e5
Himanshu Anand <anand.himanshu17@gmail.com>
2023-03-13 21:29:03 +0000

    updating to use my repo for cloning

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -704,7 +704,7 @@ if ! [ "$myTPOT_DEPLOYMENT_TYPE" == "iso" ];
   then
     fuBANNER "Cloning T-Pot"
     ### DEV
-    git clone https://github.com/telekom-security/tpotce /opt/tpot
+    git clone https://github.com/unknownhad/tpotce /opt/tpot
 fi
 
 # Let's create the T-Pot user
